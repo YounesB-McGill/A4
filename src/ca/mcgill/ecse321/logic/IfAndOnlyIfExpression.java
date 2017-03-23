38d409ce71437acc6e9d9a8c5f42c67315a2ac23
LAPTOP-HE4MJKRT\Paul Fournier <fournier.phk@gmail.com>
2017-03-23 19:52:21 -0400

    Logic

--- src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
+++ src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
@@ -7,11 +7,28 @@ public class IfAndOnlyIfExpression extends BinaryExpression implements TLExpress
 	public IfAndOnlyIfExpression(TLExpression leftExpr, TLExpression rightExpr) {
 		super(leftExpr, rightExpr);
 	}
-
+	//L <==> R == (L => R) AND (R => L)
 	@Override
 	public boolean evaluate(List<? extends State> trajectory) {
 		// TODO Create this method
-		return false;
+		if(imply(leftExpr, rightExpr) && imply(leftExpr, rightExpr)){
+			return true;
+		} else 
+			return false;		
 	}
 
+	public boolean imply(TLExpression leftExpr, TLExpression rightExpr) {
+		if(/* leftExpre == TRUE && rightExpr == TRUE */){
+			return true;
+		} else if(/* leftExpre == TRUE && rightExpr == FALSE */){
+			return false;
+		} else if(/* leftExpre == FALSE && rightExpr == TRUE */){
+			return true;
+		} else if(/* leftExpre == FALSE && rightExpr == FALSE */){
+			return true;
+		} else {
+			// throw new InvalidExpression();
+		}
+
+	}
 }
