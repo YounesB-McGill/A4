8ea9fb7a84947c25d16a76d83efc692e4fcdfffc
Adrien Sauvestre <sauvestre.adrien@free.fr>
2017-03-27 19:03:59 -0400

    Implementation of UntilExpression

--- src/ca/mcgill/ecse321/logic/UntilExpression.java
+++ src/ca/mcgill/ecse321/logic/UntilExpression.java
@@ -8,10 +8,21 @@ public class UntilExpression extends BinaryExpression implements TLExpression {
 		super(leftExpr, rightExpr);
 	}
 	
+	// For 0 until the time right is true - 1, left must be true
 	@Override
 	public boolean evaluate(List<? extends State> trajectory) {
-		// TODO Create this method
-		return false;
+		for(int i=0; i<trajectory.size();i++) {
+			if(rightExpression.evaluate(trajectory.subList(i, trajectory.size()-1))) {
+				// right has been reached and left was never false before that time
+				return true;
+			}
+			if(!leftExpression.evaluate(trajectory.subList(i, trajectory.size()-1))) {
+				// right has not been reached and left is false
+				return false;
+			}
+		}
+		// right is never reached and left is always true, so I guess we return true
+		return true;
 	}
 
 }
