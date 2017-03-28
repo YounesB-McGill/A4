5f17c484797e2c57ad72e08140db4665111ff6bf
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-28 19:37:03 -0400

    Removed Un-Needed TODO String and Comment
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
+++ src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
@@ -10,9 +10,7 @@ public class IfAndOnlyIfExpression extends BinaryExpression implements TLExpress
 
 	@Override
 	public boolean evaluate(List<? extends State> trajectory) {
-		// TODO Create this method
 		// (a <-> b) = (a -> b) AND (b -> a)
-		// (x -> y) = NOT x OR Y
 		TLExpression left = this.getLeftExpression();
 		TLExpression right = this.getRightExpression();
 		if(left != null && right != null){
