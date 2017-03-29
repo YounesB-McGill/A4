9e2375b04ceb9ab5135720e9f94748783442cf9e
Max Musing <max.musing@gmail.com>
2017-03-29 13:33:40 -0400

    Implement GloballyExpression evaluate method

--- src/ca/mcgill/ecse321/logic/GloballyExpression.java
+++ src/ca/mcgill/ecse321/logic/GloballyExpression.java
@@ -10,8 +10,30 @@ public class GloballyExpression extends UnaryExpression implements TLExpression 
 	
 	@Override
 	public boolean evaluate(List<? extends State> trajectory) {
-		// TODO Create this method
-		return false;
+		TLExpression subExpression = this.getExpression();
+		
+		// if the expression is null, return false
+		if (subExpression == null) {
+			return false;
+		}
+		else {
+			// determine the value of the trajectory
+			boolean base = subExpression.evaluate(trajectory);
+			int length = trajectory.size();
+			
+			// check if the value of the trajectory is equal throughout each subtrajectory
+			for (int i = 0; i < length; i++) {
+				List<? extends State> subTrajectory = trajectory.subList(i, length);
+				
+				// if not, return false (does not hold somewhere along the trajectory)
+				if (subExpression.evaluate(subTrajectory) != base) {
+					return false;
+				}
+			}
+			
+			// if so, return true (holds globally)
+			return true;
+		}
 	}
 
 }
