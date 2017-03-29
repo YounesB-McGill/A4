d990e0867c2c0df874376c5fa53414a5e44d9f7f
LAPTOP-HE4MJKRT\Paul Fournier <fournier.phk@gmail.com>
2017-03-29 13:34:46 -0400

    Added documentation and NP

--- src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
+++ src/ca/mcgill/ecse321/logic/IfAndOnlyIfExpression.java
@@ -7,22 +7,25 @@ public class IfAndOnlyIfExpression extends BinaryExpression implements TLExpress
 	public IfAndOnlyIfExpression(TLExpression leftExpr, TLExpression rightExpr) {
 		super(leftExpr, rightExpr);
 	}
+
 	//L <==> R == (L => R) AND (R => L)
 	@Override
 	public boolean evaluate(List<? extends State> trajectory) {
-		// TODO Create this method
 		TLExpression leftExpr = this.getLeftExpression();
 		TLExpression rightExpr = this.getRightExpression();
+		// Call a helper method that evaluates the 'implies' boolean logic operator
+		// AND the returns of the two calls to obtain the if and only if boolean logic.
 		if(imply(leftExpr, rightExpr, trajectory) && imply(rightExpr, leftExpr, trajectory)){
 			return true;
 		} else 
 			return false;		
 	}
 
+	// L => R == (!L) OR R
 	public boolean imply(TLExpression leftExpr, TLExpression rightExpr, List<? extends State> trajectory) {
 		NotExpression notLeftExpr = new NotExpression(leftExpr);
 		OrExpression implies = new OrExpression(notLeftExpr, rightExpr);
-		
+		// Return the not left or right
 		return implies.evaluate(trajectory);
 	}
 }
